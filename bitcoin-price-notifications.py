4ac5d14f8889fa23c750fe138fd4a10dd255a14d
prakhar singhal <32975416+beggh@users.noreply.github.com>
2021-01-18 16:50:05 +0530

    Add files via upload

---
+++ bitcoin-price-notifications.py
@@ -0,0 +1,89 @@
+from requests import request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
+import time
+from datetime import datetime
+
+BITCOIN_PRICE_THRESHOLD = 36000
+#BITCOIN_API_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest'
+IFTTT_WEBHOOKS_URL = 'https://maker.ifttt.com/trigger/test/with/key/m-R9I9YI6yw9lytybDH5M'
+
+
+def get_latest_bitcoin_price():
+    parameters = {
+        'symbol': 'BTC'
+    }
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': 'a4f20e04-f6c0-4d21-9e15-a69d33a9c924',
+    }
+
+    session = Session()
+    session.headers.update(headers)
+
+
+    response = session.get(URL, params=parameters)
+    data = json.loads(response.text)
+    #print(data)
+    parseData = json.dumps(response.json())
+    #print(parseData)
+    btcObj = json.loads(parseData)
+    #print(btcObj["data"]["BTC"]["name"])
+    #print(btcObj["data"]["BTC"]["quote"]["USD"]["price"])
+
+    ethString = str(btcObj["data"]["BTC"]["quote"]["USD"]["price"])
+    #print(ethString)
+    return(float(ethString))
+
+
+def post_ifttt_webhook(event, value):
+    # The payload that will be sent to IFTTT service
+    data = {'value1': value}
+    # inserts our desired event
+    ifttt_event_url = IFTTT_WEBHOOKS_URL.format(event)
+    # Sends a HTTP POST request to the webhook URL
+    request.post(ifttt_event_url, json=data)
+
+
+def format_bitcoin_history(bitcoin_history):
+    rows = []
+    for bitcoin_price in bitcoin_history:
+        # Formats the date into a string: '24.02.2018 15:09'
+        date = bitcoin_price['date'].strftime('%d.%m.%Y %H:%M')
+        price = bitcoin_price['price']
+        # <b> (bold) tag creates bolded text
+        # 24.02.2018 15:09: $<b>10123.4</b>
+        row = '{}: $<b>{}</b>'.format(date, price)
+        rows.append(row)
+
+    # Use a <br> (break) tag to create a new line
+    # Join the rows delimited by <br> tag: row1<br>row2<br>row3
+    return '<br>'.join(rows)
+
+
+def main():
+    bitcoin_history = []
+    while True:
+        price = get_latest_bitcoin_price()
+        date = datetime.now()
+        bitcoin_history.append({'date': date, 'price': price})
+
+        # Send an emergency notification
+        if price < BITCOIN_PRICE_THRESHOLD:
+            post_ifttt_webhook('bitcoin_price_emergency', price)
+
+        # Send a Telegram notification
+        if len(bitcoin_history) == 5:  # Once we have 5 items in our bitcoin_history send an update
+            post_ifttt_webhook('bitcoin_price_update',
+                               format_bitcoin_history(bitcoin_history))
+            # Reset the history
+            bitcoin_history = []
+
+        # Sleep for 5 minutes (for testing purposes you can set it to a lower number)
+        time.sleep(1 * 60)
+
+
+
+if __name__ == '__main__':
+    main()
